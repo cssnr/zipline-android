10d4db699d5fd5595f9d3bd1a96ea7e6fd4c9cae
Shane <6071159+smashedr@users.noreply.github.com>
2025-04-12 12:39:00 -0700

    Update SettingsFragment.kt and Icons

--- app/src/main/java/org/cssnr/zipline/SettingsFragment.kt
+++ app/src/main/java/org/cssnr/zipline/SettingsFragment.kt
@@ -2,32 +2,34 @@ package org.cssnr.zipline
 
 import android.os.Bundle
 import android.util.Log
-import androidx.preference.EditTextPreference
+import androidx.preference.SwitchPreferenceCompat
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceManager
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
-    companion object {
-        private const val PREFS_NAME = "default_preferences"
-    }
-
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         Log.d("SettingsFragment", "onCreatePreferences rootKey: $rootKey")
 
+        preferenceManager.sharedPreferencesName = "default_preferences"
         setPreferencesFromResource(R.xml.preferences, rootKey)
 
-        val savedUrlPref = findPreference<EditTextPreference>("saved_url")
-        Log.d("SettingsFragment", "savedUrlPref: $savedUrlPref")
+        val showPreviewPref = findPreference<SwitchPreferenceCompat>("show_preview")
+        Log.d("SettingsFragment", "showPreviewPref: $showPreviewPref")
 
-        //val preferences = context?.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-        val preferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
-        Log.d("SettingsFragment", "preferences: $preferences")
+        showPreviewPref?.setOnPreferenceChangeListener { _, newValue ->
+            val newBool = newValue as Boolean
+            Log.d("showPreviewPref", "show_preview: $newBool")
+            true
+        }
 
-        var showPreview = preferences?.getBoolean("show_preview", false)
-        Log.d("SettingsFragment", "showPreview: $showPreview")
-        var enableBiometrics = preferences?.getBoolean("enable_biometrics", false)
-        Log.d("SettingsFragment", "enableBiometrics: $enableBiometrics")
+        //val preferences = context?.getSharedPreferences("default_preferences", Context.MODE_PRIVATE)
+        //val preferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        //Log.d("SettingsFragment", "preferences: $preferences")
+
+        //var showPreview = preferences?.getBoolean("show_preview", false)
+        //Log.d("SettingsFragment", "showPreview: $showPreview")
+        //var enableBiometrics = preferences?.getBoolean("enable_biometrics", false)
+        //Log.d("SettingsFragment", "enableBiometrics: $enableBiometrics")
 
         //PreferenceManager.getDefaultSharedPreferences(requireContext())
         //    .edit { putString("saved_url", newUrl) }
