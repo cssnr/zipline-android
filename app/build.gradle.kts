5ff4037888e28ae0346e648201196c520cf6f8dc
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2025-05-29 19:26:05 -0700

    Fix Release Build (#18)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.google.services)
     //alias(libs.plugins.ksp)
+    alias(libs.plugins.google.services)
     alias(libs.plugins.firebase.crashlytics)
 }
 
