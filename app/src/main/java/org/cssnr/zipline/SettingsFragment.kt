6816cca6160b237d981d9936c02eac62779bac11
Shane <6071159+smashedr@users.noreply.github.com>
2025-05-31 13:21:36 -0700

    Make toggleAnalytics a function

--- app/src/main/java/org/cssnr/zipline/SettingsFragment.kt
+++ app/src/main/java/org/cssnr/zipline/SettingsFragment.kt
@@ -59,26 +59,10 @@ class SettingsFragment : PreferenceFragmentCompat() {
         launcherAction?.summaryProvider = ListPreference.SimpleSummaryProvider.getInstance()
 
         // Toggle Analytics
-        val toggleAnalytics = findPreference<SwitchPreferenceCompat>("analytics_enabled")
-        toggleAnalytics?.setOnPreferenceChangeListener { _, newValue ->
-            Log.d("toggleAnalytics", "analytics_enabled: $newValue")
-            if (newValue as Boolean) {
-                Log.d("toggleAnalytics", "ENABLE Analytics")
-                Firebase.analytics.setAnalyticsCollectionEnabled(true)
-                toggleAnalytics.isChecked = true
-            } else {
-                MaterialAlertDialogBuilder(ctx)
-                    .setTitle("Please Reconsider")
-                    .setMessage("Analytics are only used to fix bugs and make improvements.")
-                    .setPositiveButton("Disable Anyway") { _, _ ->
-                        Log.d("toggleAnalytics", "DISABLE Analytics")
-                        Firebase.analytics.logEvent("disable_analytics", null)
-                        Firebase.analytics.setAnalyticsCollectionEnabled(false)
-                        toggleAnalytics.isChecked = false
-                    }
-                    .setNegativeButton("Cancel", null)
-                    .show()
-            }
+        val analyticsEnabled = findPreference<SwitchPreferenceCompat>("analytics_enabled")
+        analyticsEnabled?.setOnPreferenceChangeListener { _, newValue ->
+            Log.d("analyticsEnabled", "analytics_enabled: $newValue")
+            ctx.toggleAnalytics(analyticsEnabled, newValue)
             false
         }
 
@@ -125,6 +109,27 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
         //PreferenceManager.getDefaultSharedPreferences(ctx)
         //    .edit { putString("saved_url", newUrl) }
+    }
+
+    fun Context.toggleAnalytics(switchPreference: SwitchPreferenceCompat, newValue: Any) {
+        Log.d("toggleAnalytics", "newValue: $newValue")
+        if (newValue as Boolean) {
+            Log.d("toggleAnalytics", "ENABLE Analytics")
+            Firebase.analytics.setAnalyticsCollectionEnabled(true)
+            switchPreference.isChecked = true
+        } else {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Please Reconsider")
+                .setMessage("Analytics are only used to fix bugs and make improvements.")
+                .setPositiveButton("Disable Anyway") { _, _ ->
+                    Log.d("toggleAnalytics", "DISABLE Analytics")
+                    Firebase.analytics.logEvent("disable_analytics", null)
+                    Firebase.analytics.setAnalyticsCollectionEnabled(false)
+                    switchPreference.isChecked = false
+                }
+                .setNegativeButton("Cancel", null)
+                .show()
+        }
     }
 
     fun Context.showFeedbackDialog() {
