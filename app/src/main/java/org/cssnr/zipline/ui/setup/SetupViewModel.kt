4faa73a865a724e2de3c7ed1066961998cd77302
Shane <6071159+smashedr@users.noreply.github.com>
2025-06-21 14:46:43 -0700

    Add Setup Fragment

---
+++ app/src/main/java/org/cssnr/zipline/ui/setup/SetupViewModel.kt
@@ -0,0 +1,10 @@
+package org.cssnr.zipline.ui.setup
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SetupViewModel : ViewModel() {
+    //val hostname = MutableLiveData<String>()
+    //val workInterval = MutableLiveData<String>("0")
+    val confettiShown = MutableLiveData<Boolean>(false)
+}
