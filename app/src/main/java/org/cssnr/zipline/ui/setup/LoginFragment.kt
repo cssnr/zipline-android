15d49d1ece7f1051cfbddf16046298156edd53a2
Shane <6071159+smashedr@users.noreply.github.com>
2025-07-30 16:47:01 -0700

    Update Login Page

--- app/src/main/java/org/cssnr/zipline/ui/setup/LoginFragment.kt
+++ app/src/main/java/org/cssnr/zipline/ui/setup/LoginFragment.kt
@@ -1,18 +1,23 @@
 package org.cssnr.zipline.ui.setup
 
 import android.animation.ObjectAnimator
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
 import android.text.Html
+import android.text.TextWatcher
 import android.text.method.LinkMovementMethod
 import android.util.Log
 import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.content.edit
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavOptions
@@ -34,6 +39,8 @@ class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: SetupViewModel by activityViewModels()
 
     private val navController by lazy { findNavController() }
     private val preferences by lazy { PreferenceManager.getDefaultSharedPreferences(requireContext()) }
@@ -104,6 +111,11 @@ class LoginFragment : Fragment() {
         binding.debugLogging.visibility = if (enableDebugLogs) View.VISIBLE else View.GONE
         binding.toggleDebugLogs.isChecked = enableDebugLogs
 
+        viewModel.totp.observe(viewLifecycleOwner) { totp ->
+            Log.i(LOG_TAG, "viewModel.totp.observe: totp: $totp")
+            if (totp) _binding?.loginCode?.visibility = View.VISIBLE
+        }
+
         if (arguments?.getString("url") != null) {
             Log.i(LOG_TAG, "url: ${arguments?.getString("url")}")
             binding.loginHostname.setText(arguments?.getString("url").toString())
@@ -131,106 +143,107 @@ class LoginFragment : Fragment() {
             navController.navigate(R.id.nav_item_settings_debug)
         }
 
-        binding.loginButton.setOnClickListener {
-            it.isEnabled = false
-            binding.loginError.visibility = View.INVISIBLE
-            val inputHost = binding.loginHostname.text.toString().trim()
-            Log.d("loginButton", "inputHost: $inputHost")
-            val host = parseHost(inputHost)
-            if (inputHost != host) {
-                binding.loginHostname.setText(host)
-            }
-            Log.d("loginButton", "host: $host")
-            val user = binding.loginUsername.text.toString().trim()
-            Log.d("loginButton", "user: $user")
-            val pass = binding.loginPassword.text.toString().trim()
-            Log.d("loginButton", "pass: $pass")
-            val code = binding.loginCode.text.toString().trim()
-            Log.d("loginButton", "code: $code")
-
-            var valid = true
-            if (host.isEmpty() || host == "https://") {
-                binding.loginHostname.error = "Required"
-                valid = false
-            }
-            if (valid && host.toHttpUrlOrNull() == null) {
-                binding.loginHostname.error = "Invalid Host"
-                valid = false
-            }
-            if (user.isEmpty()) {
-                binding.loginUsername.error = "Required"
-                valid = false
-            }
-            if (pass.isEmpty()) {
-                binding.loginPassword.error = "Required"
-                valid = false
-            }
-            if (!valid) {
-                it.isEnabled = true
-                return@setOnClickListener
+        binding.loginCode.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("beforeTextChanged", "s: $s - start=$start, count=$count, after=$after")
             }
 
-            Log.d("loginButton", "lifecycleScope.launch")
-            lifecycleScope.launch {
-                val api = ServerApi(ctx, host)
-                val auth: LoginData = api.login(host, user, pass, code)
-                Log.d("loginButton", "auth: $auth")
-                if (auth.error != null) {
-                    Log.d("loginButton", "LOGIN FAILED")
-                    _binding?.loginError?.visibility = View.VISIBLE
-                    _binding?.loginError?.text = auth.error
-                    val shake = ObjectAnimator.ofFloat(
-                        _binding?.loginButton, "translationX",
-                        0f, 25f, -25f, 20f, -20f, 15f, -15f, 6f, -6f, 0f
-                    )
-                    shake.duration = 800
-                    shake.start()
-                    val red =
-                        ContextCompat.getColor(requireContext(), android.R.color.holo_red_dark)
-                    val original = ContextCompat.getColor(requireContext(), R.color.button_color)
-                    _binding?.loginButton?.setBackgroundColor(red)
-                    viewLifecycleOwner.lifecycleScope.launch {
-                        delay(700)
-                        if (viewLifecycleOwner.lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
-                            _binding?.loginButton?.setBackgroundColor(original)
-                        }
-                    }
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                        _binding?.loginButton?.performHapticFeedback(HapticFeedbackConstants.REJECT)
-                    } else {
-                        _binding?.loginButton?.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
-                    }
-                    Firebase.analytics.logEvent("login_failed", null)
-                } else {
-                    Log.d("loginButton", "LOGIN SUCCESS")
-                    preferences.edit {
-                        putString("ziplineUrl", host)
-                        putString("ziplineToken", auth.token)
-                    }
-                    Log.d("loginButton", "ziplineUrl: $host")
-                    Log.d("loginButton", "ziplineToken: ${auth.token}")
-                    Firebase.analytics.logEvent("login_success", null)
-                    //GlobalScope.launch(Dispatchers.IO) { ctx.updateStats() }
-                    // TODO: Consider managing first run logic in MainActivity...
-                    if (!preferences.getBoolean("first_run_shown", false)) {
-                        preferences.edit { putBoolean("first_run_shown", true) }
-                        navController.navigate(
-                            R.id.nav_action_login_setup, null, NavOptions.Builder()
-                                .setPopUpTo(navController.graph.id, true)
-                                .build()
-                        )
-                    } else {
-                        navController.navigate(
-                            navController.graph.startDestinationId, null, NavOptions.Builder()
-                                .setPopUpTo(navController.graph.id, true)
-                                .build()
-                        )
-                    }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("onTextChanged", "s: $s - start=$start, before=$before, count=$count")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("afterTextChanged", "${s?.length} - $s")
+                if (s?.length == 6) {
+                    lifecycleScope.launch { ctx.processLogin() }
                 }
-                it.isEnabled = true
-                Log.d("loginButton", "lifecycleScope: DONE")
+            }
+        })
+
+        binding.loginButton.setOnClickListener {
+            lifecycleScope.launch { ctx.processLogin() }
+        }
+    }
+
+    private suspend fun Context.processLogin() {
+        _binding?.loginButton?.isEnabled = false
+        _binding?.loginError?.visibility = View.INVISIBLE
+        val inputHost = _binding?.loginHostname?.text.toString().trim()
+        Log.d("loginButton", "inputHost: $inputHost")
+        val host = parseHost(inputHost)
+        if (inputHost != host) {
+            _binding?.loginHostname?.setText(host)
+        }
+        Log.d("loginButton", "host: $host")
+        val user = _binding?.loginUsername?.text.toString().trim()
+        Log.d("loginButton", "user: $user")
+        val pass = _binding?.loginPassword?.text.toString().trim()
+        Log.d("loginButton", "pass: $pass")
+        val code = _binding?.loginCode?.text.toString().trim()
+        Log.d("loginButton", "code: $code")
+
+        var valid = true
+        if (host.isEmpty() || host == "https://") {
+            _binding?.loginHostname?.error = "Required"
+            valid = false
+        }
+        if (valid && host.toHttpUrlOrNull() == null) {
+            _binding?.loginHostname?.error = "Invalid Host"
+            valid = false
+        }
+        if (user.isEmpty()) {
+            _binding?.loginUsername?.error = "Required"
+            valid = false
+        }
+        if (pass.isEmpty()) {
+            _binding?.loginPassword?.error = "Required"
+            valid = false
+        }
+        if (!valid) {
+            _binding?.loginButton?.isEnabled = true
+            return
+        }
+
+        val api = ServerApi(this, host)
+        val auth: LoginData = api.login(host, user, pass, code)
+        Log.d("loginButton", "auth: $auth")
+        if (auth.totp == true) {
+            Log.d("loginButton", "TOTP REQUIRED")
+            showErrorAnimation(_binding?.loginButton, _binding?.loginError, "Two-Factor Required")
+            viewModel.totp.value = true
+            _binding?.loginCode?.requestFocus()
+        } else if (auth.error != null) {
+            Log.d("loginButton", "LOGIN FAILED")
+            showErrorAnimation(_binding?.loginButton, _binding?.loginError, auth.error)
+            Firebase.analytics.logEvent("login_failed", null)
+        } else {
+            Log.d("loginButton", "LOGIN SUCCESS")
+            preferences.edit {
+                putString("ziplineUrl", host)
+                putString("ziplineToken", auth.token)
+            }
+            Log.d("loginButton", "ziplineUrl: $host")
+            Log.d("loginButton", "ziplineToken: ${auth.token}")
+            Firebase.analytics.logEvent("login_success", null)
+            //GlobalScope.launch(Dispatchers.IO) { ctx.updateStats() }
+            // TODO: Consider managing first run logic in MainActivity...
+            if (!preferences.getBoolean("first_run_shown", false)) {
+                preferences.edit { putBoolean("first_run_shown", true) }
+                navController.navigate(
+                    R.id.nav_action_login_setup, null, NavOptions.Builder()
+                        .setPopUpTo(navController.graph.id, true)
+                        .build()
+                )
+            } else {
+                navController.navigate(
+                    navController.graph.startDestinationId, null, NavOptions.Builder()
+                        .setPopUpTo(navController.graph.id, true)
+                        .build()
+                )
             }
         }
+        binding.loginButton.isEnabled = true
+        Log.d("loginButton", "lifecycleScope: DONE")
     }
 
     private fun parseHost(urlString: String): String {
@@ -248,5 +261,35 @@ class LoginFragment : Fragment() {
             url = url.substring(0, url.length - 1)
         }
         return url
+    }
+
+    fun showErrorAnimation(buttonView: View?, textView: TextView? = null, text: String? = null) {
+        Log.d("loginFailed", "Context.loginFailed")
+        textView.let {
+            it?.text = text
+            it?.visibility = View.VISIBLE
+        }
+        val shake = buttonView.let {
+            ObjectAnimator.ofFloat(
+                it, "translationX",
+                0f, 25f, -25f, 20f, -20f, 15f, -15f, 6f, -6f, 0f
+            )
+        }
+        shake?.duration = 800
+        shake?.start()
+        val red = ContextCompat.getColor(requireContext(), android.R.color.holo_red_dark)
+        val original = ContextCompat.getColor(requireContext(), R.color.button_color)
+        buttonView?.setBackgroundColor(red)
+        lifecycleScope.launch {
+            delay(700)
+            if (lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)) {
+                buttonView?.setBackgroundColor(original)
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            buttonView?.performHapticFeedback(HapticFeedbackConstants.REJECT)
+        } else {
+            buttonView?.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
+        }
     }
 }
