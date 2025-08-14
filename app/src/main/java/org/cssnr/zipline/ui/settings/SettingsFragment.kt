17a3f6c31ce061abdfa59197ae386877ed4b77fd
Shane <6071159+smashedr@users.noreply.github.com>
2025-08-14 16:08:16 -0700

    Update Firebase

--- app/src/main/java/org/cssnr/zipline/ui/settings/SettingsFragment.kt
+++ app/src/main/java/org/cssnr/zipline/ui/settings/SettingsFragment.kt
@@ -28,8 +28,8 @@ import androidx.preference.SwitchPreferenceCompat
 import androidx.work.WorkManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.analytics.analytics
+import com.google.firebase.Firebase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
