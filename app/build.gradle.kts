632552b843df9a511b1e0372c840de3824e8d7ac
Shane <6071159+smashedr@users.noreply.github.com>
2026-09-18 20:24:39 -0700

    Update AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,9 @@ configure<ApplicationExtension> {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            optimization {
+                enable = true
+            }
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
